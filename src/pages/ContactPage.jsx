08edb8be87520003cdadc4110092add70f3a44ad
skinsar <hernanbarrera427@gmail.com>
2025-10-01 14:58:32 -0300

    Actualizar ContactPage.jsx

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -1,6 +1,6 @@
 import { useEffect } from 'react';
 import ContactForm from '../components/ContactForm';
-import BlurText from '../components/BlurText'; // <-- Importamos el componente
+import BlurText from '../components/BlurText';
 
 const ContactPage = () => {
   useEffect(() => {
@@ -9,7 +9,7 @@ const ContactPage = () => {
 
   return (
     <div className="section-container">
-      {/* 👇 Reemplazamos el h1 por BlurText 👇 */}
+      
       <BlurText text="Contacto y Pedidos" className="section-title" />
       <p className="text-center text-lg text-gray-400 max-w-2xl mx-auto mb-12">
         Tu voz es parte de Universo 8090. Envíame tus pedidos musicales, dedicatorias, o cualquier mensaje que quieras compartir.
